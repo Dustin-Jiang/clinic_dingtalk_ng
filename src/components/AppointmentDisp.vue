011721f5694ead4cd75721be7d6704b8efacd82a
Dustin-Jiang <dustinjiang@outlook.com>
2026-01-18 15:06:36 +0800

    添加拒绝原因显示

--- src/components/AppointmentDisp.vue
+++ src/components/AppointmentDisp.vue
@@ -1,22 +1,22 @@
 <template>
   <n-space vertical>
     <RecordItem :data="props.data" />
-    <div
-      v-if="isQrCodeVisible"
-      style="justify-content: center; align-items: center; flex-direction: column; display: flex"
-    >
+    <div v-if="isQrCodeVisible"
+      style="justify-content: center; align-items: center; flex-direction: column; display: flex">
       <div style="padding: 8px 0 0 0">
-        <n-qr-code
-          :value="props.data!.url"
-          :size="120"
-          :error-correction-level="'Q'"
-          style="padding: 8px; box-sizing: content-box"
-        />
+        <n-qr-code :value="props.data!.url" :size="120" :error-correction-level="'Q'"
+          style="padding: 8px; box-sizing: content-box" />
       </div>
       <n-text style="font-size: 2rem; font-weight: 600">
         {{ props.data!.id }}
       </n-text>
       <n-text>请在到达诊所后向工作人员展示</n-text>
+    </div>
+    <div v-if="isRejectReasonVisible">
+      <div>
+        <n-text strong>拒绝原因</n-text>
+      </div>
+      <n-text>{{ props.data!.reject_reason }}</n-text>
     </div>
   </n-space>
 </template>
@@ -37,5 +37,10 @@ const isQrCodeVisible = computed(() => {
     props.data!.status == RecordStatus.PROCESS_PENDING ||
     props.data!.status == RecordStatus.RESOLVING
   )
+})
+
+const isRejectReasonVisible = computed(() => {
+  return props.data!.status == RecordStatus.APPOINTMENT_REJECTED &&
+    !!props.data!.reject_reason
 })
 </script>
